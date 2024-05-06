1c8d4f5e4c9ce5cd1bc90b9604371a0f35c8f12a
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-05-06 09:22:54 +0200

    backend fonctionnel (voir description)
    il faut bien faire npm install et npm install sequelize car node modules est ignoré sur le repo + créer une db "db_passion_lecture" en local (vide car elle sera remplie au lancement avec les models)

--- 295-passionLecture/Back-end/src/db/sequelize.mjs
+++ 295-passionLecture/Back-end/src/db/sequelize.mjs
@@ -1,36 +1,37 @@
-import { Sequelize, DataTypes } from "sequelize";
-import bcrypt from "bcrypt";
+import { Sequelize, DataTypes } from 'sequelize'
+import bcrypt from 'bcrypt'
 
-import { BookModel } from "../models/books.mjs";
-import { AuthorModel } from "../models/authors.mjs";
-import { CategoryModel } from "../models/categories.mjs";
-import { UserModel } from "../models/users.mjs";
-import { CommentModel } from "../models/comments.mjs";
-import { NoteModel } from "../models/notes.mjs";
+import { BookModel } from '../models/books.mjs'
+import { AuthorModel } from '../models/authors.mjs'
+import { CategoryModel } from '../models/categories.mjs'
+import { UserModel } from '../models/users.mjs'
+import { CommentModel } from '../models/comments.mjs'
+import { NoteModel } from '../models/notes.mjs'
 
-import { books } from "./mock-book.mjs";
-import { users } from "./mock-user.mjs";
+import { books } from './mock-book.mjs'
+import { users } from './mock-user.mjs'
 
 const sequelize = new Sequelize(
-  "db_passion_lecture", // Nom de la DB qui doit exister
-  "root", // Nom de l'utilisateur
-  "root", // Mot de passe de l'utilisateur
+  'db_passion_lecture', // Nom de la DB qui doit exister
+  'root', // Nom de l'utilisateur
+  'root', // Mot de passe de l'utilisateur
   {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false,
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 6033,
+    logging: false
   }
-);
+)
 
 // Les modèles sont instanciés ici afin de
 // - pouvoir créer les tables correspondantes dans MySQL grâce à la synchronisation
 // - les exporter
-const Book = BookModel(sequelize, DataTypes);
-const Author = AuthorModel(sequelize, DataTypes);
-const Category = CategoryModel(sequelize, DataTypes);
-const User = UserModel(sequelize, DataTypes);
-const Comment = CommentModel(sequelize, DataTypes);
-const Note = NoteModel(sequelize, DataTypes);
+const Book = BookModel(sequelize, DataTypes)
+const Author = AuthorModel(sequelize, DataTypes)
+const Category = CategoryModel(sequelize, DataTypes)
+const User = UserModel(sequelize, DataTypes)
+const Comment = CommentModel(sequelize, DataTypes)
+const Note = NoteModel(sequelize, DataTypes)
 
 const models = {
   Book,
@@ -38,31 +39,26 @@ const models = {
   Category,
   User,
   Comment,
-  Note,
-};
+  Note
+}
 
 // Définition des associations
 Object.keys(models).forEach((modelName) => {
   if (models[modelName].associate) {
-    models[modelName].associate(models);
+    models[modelName].associate(models)
   }
-});
+})
 
 let initDb = async () => {
   try {
-    await sequelize.sync({ force: true });
-    await importBooks();
-    await importUsers();
-    console.log(
-      "La base de données db_passion_lecture a bien été synchronisée"
-    );
+    await sequelize.sync({ force: true })
+    await importBooks()
+    await importUsers()
+    console.log('La base de données db_passion_lecture a bien été synchronisée')
   } catch (error) {
-    console.error(
-      "Erreur lors de l'initialisation de la base de données :",
-      error
-    );
+    console.error("Erreur lors de l'initialisation de la base de données :", error)
   }
-};
+}
 
 const importBooks = async () => {
   for (const book of books) {
@@ -70,16 +66,16 @@ const importBooks = async () => {
     const [author, authorCreated] = await Author.findOrCreate({
       where: {
         lastname: book.author.lastname,
-        firstname: book.author.firstname,
-      },
-    });
+        firstname: book.author.firstname
+      }
+    })
 
     // Créer ou trouver la catégorie avant de créer le livre
     const [category, categoryCreated] = await Category.findOrCreate({
       where: {
-        name: book.category,
-      },
-    });
+        name: book.category
+      }
+    })
 
     // Création du nouveau livre
     const newBook = await Book.create({
@@ -92,24 +88,24 @@ const importBooks = async () => {
       imagePath: book.imagePath,
       categoryId: category.id,
       authorId: author.id,
-      userId: book.userId,
-    });
+      userId: book.userId
+    })
 
     // console.log(newBook.toJSON());
   }
-  console.log("Tous les livres ont été importés avec succès.");
-};
+  console.log('Tous les livres ont été importés avec succès.')
+}
 
 const importUsers = async () => {
   for (const user of users) {
-    const hash = await bcrypt.hash(user.password, 10); // temps pour hasher = du sel
+    const hash = await bcrypt.hash(user.password, 10) // temps pour hasher = du sel
     const createdUser = await User.create({
       username: user.username,
       password: hash,
-      isAdmin: user.isAdmin,
-    });
+      isAdmin: user.isAdmin
+    })
     // console.log(createdUser.toJSON());
   }
-};
+}
 
-export { sequelize, initDb, Book, Author, Category, User, Comment, Note };
+export { sequelize, initDb, Book, Author, Category, User, Comment, Note }
