a14b8b4830b0e6eafbf7ad16a68770bcff263d1c
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-05-13 12:00:10 +0200

    page de commentaires/notes
    static, n'est pas accessible via une carte directement pour l'instant

---
+++ 295-passionLecture/src/views/Addcomment.vue
@@ -0,0 +1,28 @@
+<script setup>
+import BookCard from '@/components/BookCard.vue'
+import CommentSection from '@/components/CommentSection.vue'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="main">
+    <div class="container">
+      <CommentSection />
+      <BookCard />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  display: flex;
+  align-items: stretch;
+  gap: 40px;
+}
+</style>
