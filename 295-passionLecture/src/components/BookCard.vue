4f2fcbdb5707bf746870f3116e2a6df8d6a2f97e
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-05-13 08:37:32 +0200

    Création d'un composant pour chaque carte

---
+++ 295-passionLecture/src/components/BookCard.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+
+<template>
+  <div class="cards">
+    <div class="card">
+      <img src="../../resource/img/harry_potter.png" />
+      <p class="title">Harry Potter à l'école des sorciers</p>
+      <p class="genre">Science-fiction</p>
+      <p class="author">J.K. Rowling</p>
+      <p class="count_pages">225 pages</p>
+      <p class="release_date">Octobre 2017</p>
+      <p class="stars">X X X X</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+:root {
+  --blue: #254441;
+  --green: #43aa8b;
+  --white: #eeeeee;
+}
+
+.cards {
+  font-size: 15px;
+  width: 50%;
+}
+
+.cards h1 {
+  text-align: center;
+}
+
+.cards .card {
+  background-color: var(--blue);
+  margin: auto;
+  color: var(--white);
+  padding: 20px;
+}
+
+.cards .card p {
+  margin: 1px 0; /* Réglez cette valeur selon vos besoins */
+}
+</style>
--- 295-passionLecture/src/views/HomeCards.vue
+++ 295-passionLecture/src/views/HomeCards.vue
@@ -1,42 +1,34 @@
+<script setup>
+import BookCard from '@/components/BookCard.vue'
+</script>
+
 <template>
-  <div class="cards">
+  <main>
     <h1>NOS RÉCENTS OUVRAGES</h1>
-    <div class="card">
-      <img src="../../resource/img/harry_potter.png" />
-      <p class="title">Harry Potter à l'école des sorciers</p>
-      <p class="genre">Science-fiction</p>
-      <p class="author">J.K. Rowling</p>
-      <p class="count_pages">225 pages</p>
-      <p class="release_date">Octobre 2017</p>
-      <p class="stars">X X X X</p>
+    <div>
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
     </div>
-  </div>
+  </main>
 </template>
 
-<style>
-:root {
-  --blue: #254441;
-  --green: #43aa8b;
-  --white: #eeeeee;
+<style scoped>
+main {
+  width: 50%;
 }
 
-@media (min-width: 1024px) {
-  .cards {
-    font-size: 20px;
-    width: 55%;
-  }
+h1 {
+  text-align: center;
+}
 
-  .cards h1 {
-    text-align: center;
-  }
-
-  .cards .card {
-    line-height: 1;
-    white-space: pre-line;
-    width: 35%;
-    background-color: var(--blue);
-    color: var(--white);
-    padding: 10px;
-  }
+div {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 100%;
+  margin: auto;
 }
 </style>
