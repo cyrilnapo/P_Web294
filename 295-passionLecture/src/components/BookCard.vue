084739465c78580e618ee88aa68725ca6d4b6fdb
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-05-17 13:40:09 +0200

    Update BookCard.vue

--- 295-passionLecture/src/components/BookCard.vue
+++ 295-passionLecture/src/components/BookCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-let star = 4
+/*let star = 4
 
 function showStars() {
   let starChar = '★ '
@@ -10,7 +10,7 @@ function showStars() {
   }
 
   return stars
-}
+}*/
 </script>
 
 <template>
@@ -18,31 +18,13 @@ function showStars() {
     <div class="img"><img :src="book.imagePath" /></div>
 
     <p class="title">{{ book.title }}</p>
-    <p class="abstract">{{ book.abstract }}</p>
-    <p class="author">{{ getAuthorName(book.authorId) }}</p>
+    <!-- <p class="abstract">{{ book.abstract }}</p> -->
+    <p class="author">auteur</p>
     <p class="count_pages">{{ book.numberOfPages }} pages</p>
     <p class="edition_year">Edition Year: {{ book.editionYear }}</p>
     <a :href="book.pdfLink" target="_blank">PDF Link</a>
   </div>
 </template>
-
-<script setup>
-import { defineProps } from 'vue'
-
-const props = defineProps({
-  book: Object // Define the type of the prop
-})
-
-.cards {
-  font-size: 14px;
-  width: 50%;
-}
-
-// Function to get author name based on authorId
-function getAuthorName(authorId) {
-  return authorMap[authorId] || 'Unknown Author'
-}
-</script>
 
 <style scoped>
 .card {
@@ -51,6 +33,11 @@ function getAuthorName(authorId) {
   color: var(--white);
   padding: 15px;
   margin: 5px 0px 0px 0px;
+}
+
+.cards {
+  font-size: 14px;
+  width: 50%;
 }
 
 .cards .card p {
