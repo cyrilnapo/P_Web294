5b24ed899574a77ef20a4bcffdb5a4242abefcaf
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-05-17 13:38:55 +0200

    Update BookCard.vue

--- 295-passionLecture/src/components/BookCard.vue
+++ 295-passionLecture/src/components/BookCard.vue
@@ -19,30 +19,12 @@ function showStars() {
 
     <p class="title">{{ book.title }}</p>
     <p class="abstract">{{ book.abstract }}</p>
-    <p class="author">{{ getAuthorName(book.authorId) }}</p>
+    <p class="author">autuer</p>
     <p class="count_pages">{{ book.numberOfPages }} pages</p>
     <p class="edition_year">Edition Year: {{ book.editionYear }}</p>
     <a :href="book.pdfLink" target="_blank">PDF Link</a>
   </div>
 </template>
-
-<script setup>
-import { defineProps } from 'vue'
-
-const props = defineProps({
-  book: Object // Define the type of the prop
-})
-
-.cards {
-  font-size: 14px;
-  width: 50%;
-}
-
-// Function to get author name based on authorId
-function getAuthorName(authorId) {
-  return authorMap[authorId] || 'Unknown Author'
-}
-</script>
 
 <style scoped>
 .card {
@@ -51,6 +33,11 @@ function getAuthorName(authorId) {
   color: var(--white);
   padding: 15px;
   margin: 5px 0px 0px 0px;
+}
+
+.cards {
+  font-size: 14px;
+  width: 50%;
 }
 
 .cards .card p {
